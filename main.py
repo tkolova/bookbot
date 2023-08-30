0d022eaf30d9519b57388824e21038155c822de1
tkolova <88898046+tkolova@users.noreply.github.com>
2023-08-30 18:08:34 +0200

    Add letter count function

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import string
+
 def main():
     book_path = "books/frankenstein.txt"
     contents = read_book(book_path)
     print_book(contents)
     count_words(contents)
+    count_letters(contents)
 
 def read_book(book_path):
     with open(book_path) as f:
@@ -20,5 +23,21 @@ def count_words(contents):
     print()
     print("=========")
     print()
+
+def count_letters(contents):
+    cont_lowercase = contents.lower()
+    alphabet = list(string.ascii_lowercase)
+    count_dict = {}
+    for letter in alphabet:
+        count_dict[letter] = 0
+    for letter in cont_lowercase:
+        if letter in count_dict:
+            count_dict[letter] += 1
+    print("The letter counts are:" + "\n")
+    print(count_dict)
+    print()
+    print("=========")
+    print()
+        
 
 main()
