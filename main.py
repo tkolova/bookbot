07ca721bb642a6a15078c6f24e4633ecbfd84869
tkolova <88898046+tkolova@users.noreply.github.com>
2023-08-30 17:18:06 +0200

    Add file read function

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("yikes")
+def main():
+    book_path = "books/frankenstein.txt"
+    contents = read_book(book_path)
+    print(contents)
+    
+
+def read_book(book_path):
+    #file_contents = None
+    with open(book_path) as f:
+        file_contents = f.read()
+    return file_contents
+
+main()
